95f29677ee467551e6194b7e486c0de37b231c7c
Alekh Sachan <137406174+alekh08@users.noreply.github.com>
2025-11-18 10:25:20 +0530

    Implement getSneakyNumbers method in Solution class

---
+++ 3289._The_Two_Sneaky_Numbers_of_Digitville.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getSneakyNumbers(self, nums: List[int]) -> List[int]:
+        seen=set()
+        res=[]
+        for i in nums:
+            if i in seen:
+                res.append(i)
+            else:
+                seen.add(i)
+        return res
+            
